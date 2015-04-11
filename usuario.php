0b8ed670f5a422dde8a4bc85eadb1e29f2d08848
estudiante <estudiante@COLMED10625625.poligran.edu.co>
2015-04-11 11:14:14 -0500

    MVC Primera clase 1

--- usuario.php
+++ usuario.php
@@ -10,8 +10,9 @@ if ($action == 'registrar') {
         
         $nombre = $_POST['nombre'];
         $edad = $_POST['edad'];
+        $ciudad = $_POST['ciudad'];
         
-        mysql_query("insert into usuario values('$nombre', '$edad')");
+        mysql_query("insert into usuario values('$nombre', '$edad', '$ciudad')");
         header("location:usuario.php?action=registrar");
     } else {
         $usuarios = mysql_query("select * from usuario");
@@ -19,6 +20,11 @@ if ($action == 'registrar') {
     }
 } else if ($action == 'editar') {
     require './vistas/usuario/editar.php';
+
+} else if ($action == 'delete') {
+    $nombre = $_GET['nombre'];
+    mysql_query("delete from usuario where nombre='$nombre'");
+    header("location:usuario.php?action=registrar");
 }
   
   
--- vistas/usuario/registrar.php
+++ vistas/usuario/registrar.php
@@ -3,6 +3,7 @@
 <form method="post" action="usuario.php?action=registrar">
     Nombre: <input type="text" name="nombre"><br>
    Edad: <input type="number" name="edad"><br>
+    Ciudad: <input type="text" name="ciudad"><br>
    <input type="submit">
    
 </form>
@@ -12,12 +13,16 @@
     <tr>
         <td>nombre</td>
         <td>edad</td>   
+        <td>Ciudad</td>
     </tr>
     
     <?php while ($data = mysql_fetch_assoc($usuarios)) { ?>
     <tr>
         <td><?php echo $data['nombre'] ?></td>
         <td><?php echo $data['edad'] ?></td>  
+        <td><?php echo $data['ciudad'] ?></td>  
+        <td><a href="">Editar</a></td>  
+        <td><a href="usuario.php?action=delete&nombre=<?php echo $data['nombre'] ?>">Eliminar</a></td>  
     </tr>
     <?php } ?>
 </table>
